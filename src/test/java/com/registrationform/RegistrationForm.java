e078a62457e26697596399b9cd341bb46bb87e7e
SRC0747 <RjSrc@0747>
2021-08-27 21:00:02 +0530

    added: Validate 10-digit mobile number in Registration Form.

--- src/test/java/com/registrationform/RegistrationForm.java
+++ src/test/java/com/registrationform/RegistrationForm.java
@@ -1,4 +1,6 @@
 package com.registrationform;
+import org.junit.jupiter.api.Test;
+
 import java.util.regex.*;
 //import java.util.regex.Matcher;
 //import java.util.regex.Pattern;
@@ -30,7 +32,7 @@ public class RegistrationForm {
         return m.matches();
     }
     // Function to validate the Second Username
-    public static boolean isValidSecondUsername(String name)
+    public static boolean isValidLastUsername(String name)
     {
 
         // Regex to check valid username.
@@ -61,6 +63,14 @@ public class RegistrationForm {
         Pattern p = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
         Matcher m = p.matcher("foobar@gmail.com");
     }
+
+    //Checking the valid 10-digit mobile number.
+    @Test
+    public boolean whenMatchesTenDigitsNumber_thenCorrect() {
+        Pattern pattern = Pattern.compile("^\\d{10}$");
+        Matcher matcher = pattern.matcher("2055550125");
+        assertTrue(matcher.matches());
+    }
     public static void main(String[] args)
     {
         // Test Case: 1
@@ -78,18 +88,21 @@ public class RegistrationForm {
         //Checking the last username.
         // Test Case: 1
         String str1 = "Kolkata22";
-        System.out.println(isValidFirstUsername(str1));
+        System.out.println(isValidLastUsername(str1));
 
         // Test Case: 2
         String str2 = "Cap36MkS";
-        System.out.println(isValidFirstUsername(str2));
+        System.out.println(isValidLastUsername(str2));
 
         // Test Case: 3
         String str3 = "CapRiya526Biny";
-        System.out.println(isValidFirstUsername(str3));
+        System.out.println(isValidLastUsername(str3));
 
         //Checking Email-id.
         String email = "sampritirc@gmail.com";
         System.out.println(isValidEmail(email));
+
+        //Checking Valid Mobile Number.
+        System.out.println(whenMatchesTenDigitsNumber_thenCorrect());
     }
 }
