3c614b11c4f2bb3ea1c4a884a3e92eabe7d17a96
SRC0747 <RjSrc@0747>
2021-08-27 14:23:28 +0530

    Registration Form

---
+++ src/test/java/com/registrationform/RegistrationForm.java
@@ -0,0 +1,10 @@
+package com.registrationform;
+import java.util.regex.*;
+
+public class RegistrationForm {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Registration Form");
+    }
+}
