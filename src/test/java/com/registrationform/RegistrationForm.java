6533fd3e06e70399fd68617b6acad86d1f21f940
SRC0747 <RjSrc@0747>
2021-08-27 20:41:36 +0530

    added: Validate Email-id of Registration Form.

--- src/test/java/com/registrationform/RegistrationForm.java
+++ src/test/java/com/registrationform/RegistrationForm.java
@@ -54,6 +54,13 @@ public class RegistrationForm {
         // matched the ReGex
         return m.matches();
     }
+
+    //Check the Valid Email-id.
+    public static boolean isValidEmail(String name)
+    {
+        Pattern p = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+        Matcher m = p.matcher("foobar@gmail.com");
+    }
     public static void main(String[] args)
     {
         // Test Case: 1
@@ -79,6 +86,10 @@ public class RegistrationForm {
 
         // Test Case: 3
         String str3 = "CapRiya526Biny";
-        System.out.println(isValidFirstUsername(str3))
+        System.out.println(isValidFirstUsername(str3));
+
+        //Checking Email-id.
+        String email = "sampritirc@gmail.com";
+        System.out.println(isValidEmail(email));
     }
 }
