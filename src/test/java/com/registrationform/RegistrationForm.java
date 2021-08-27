5b649f4db3cb2dc59f9218f040629fd6550f0e86
SRC0747 <87673329+SRC0747@users.noreply.github.com>
2021-08-27 20:50:54 +0530

    Update RegistrationForm.java

--- src/test/java/com/registrationform/RegistrationForm.java
+++ src/test/java/com/registrationform/RegistrationForm.java
@@ -78,15 +78,15 @@ public class RegistrationForm {
         //Checking the last username.
         // Test Case: 1
         String str1 = "Kolkata22";
-        System.out.println(isValidFirstUsername(str1));
+        System.out.println(isValidSecondUsername(str1));
 
         // Test Case: 2
         String str2 = "Cap36MkS";
-        System.out.println(isValidFirstUsername(str2));
+        System.out.println(isValidSecondUsername(str2));
 
         // Test Case: 3
         String str3 = "CapRiya526Biny";
-        System.out.println(isValidFirstUsername(str3));
+        System.out.println(isValidSecondUsername(str3));
 
         //Checking Email-id.
         String email = "sampritirc@gmail.com";
